fd81a2d7ddabaf57caa2d70d47fb2c5ac077f25a
xiangbingze <xiangbingze@gmail.com>
2024-05-27 18:21:59 +0800

    feat(analytics): 增加google analytics

--- src/components/trace/trace.tsx
+++ src/components/trace/trace.tsx
@@ -1,11 +1,14 @@
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export function Trace() {
+    const googleGaId = process.env.ANALYTICS_GOOGLE_GAID;
   return (
     <>
       <Analytics />;
       <SpeedInsights />
+      {googleGaId && <GoogleAnalytics gaId={googleGaId}/>}
     </>
   );
 }
